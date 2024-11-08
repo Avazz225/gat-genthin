6e73741d9033886b280a79359ef8ba1ee5acde75
Jan Jeschinski <jeschinski.jan@t-online.de>
2024-11-08 19:26:40 +0100

    add raffle

--- raffle_server/files/sqlQueries.py
+++ raffle_server/files/sqlQueries.py
@@ -1,5 +1,6 @@
 class SQLInsert:
     CREATE_PARTICIPATION = """INSERT INTO participants (firstName, lastName, email, code) VALUES ('%s','%s','%s','%s');"""
 
+
 class SQLUpdate:
     VERIFY_EMAIL = """UPDATE participants SET verified='1' WHERE code = '%s';"""
